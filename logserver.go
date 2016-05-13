99490eee18296880320874e4b18e4f62de3629d4
Zongmin Lei <leizongmin@gmail.com>
2016-05-13 11:55:05 +0800

    支持命令行参数 -listen 指定监听地址

--- logserver.go
+++ logserver.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"LogServer/server"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 )
 
+var listenPath = flag.String("listen", ":8080", `server listen path, e.g. ":8080" or "/var/run/logserver.sock"`)
+
+func usage() {
+
+	fmt.Printf("%s\n", `
+Usage: logserver [options]
+
+Options:
+	`)
+
+	flag.PrintDefaults()
+	os.Exit(0)
+
+}
+
 func main() {
+
+	flag.Usage = usage
+	flag.Parse()
+
+	start()
+
+}
+
+func start() {
 
 	log.Printf("PID: %d", os.Getpid())
 
-	addr := "/tmp/logserver.sock"
-	//addr := ":8080"
+	addr := *listenPath
 	log.Printf("Listen: trying to listen on %s\n", addr)
 
 	err := server.Listen(addr)
